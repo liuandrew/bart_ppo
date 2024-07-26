8f3f5281c738c1238b020cf13afe61fe5bc3200a
liuandrew <14andrewliu@gmail.com>
2024-07-25 20:26:06 -0700

    Fully fledged BartMetaEnv

--- env/gym_bart/envs/bart_meta_env.py
+++ env/gym_bart/envs/bart_meta_env.py
@@ -10,7 +10,7 @@ class BartMetaEnv(gym.Env):
     def __init__(self, colors_used=3, toggle_task=True,
                  give_last_action=True, give_size=True,
                  inflate_speed=0.05, inflate_noise=0.02, rew_on_pop=0,
-                 pop_noise=0.05, max_steps=200):
+                 pop_noise=0.05, max_steps=2000, meta_setup=0):
         """
         Action space: 3 actions
             toggle_task: if True, action 1 inflates, action 0 lets go
@@ -26,19 +26,23 @@ class BartMetaEnv(gym.Env):
             inflate_noise: std of Gaussian noise added to inflation
             pop_noise: std of Gaussian noise added to pop time
             rew_on_pop: reward given if balloon pops (set to negative for punishment)
+            
+            meta_setup:
+                0: set balloon mean sizes to be ordered red/orange
+                1: set balloon mean sizes to be anything between [0.1, 1]
         """
         super(BartMetaEnv, self).__init__()
 
         self.colors = {
             "red": {"mean": 0.2},
-            "yellow": {"mean": 0.5},
-            "orange": {"mean": 0.8},
+            "orange": {"mean": 0.5},
+            "yellow": {"mean": 0.8},
             "gray": {"fixed_reward": 0},
             "purple": {"fixed_reward": 1}
         }
-        self.color_to_idx = {"red": 0, "yellow": 1, "orange": 2,
+        self.color_to_idx = {"red": 0, "orange": 1, "yellow": 2,
                              "gray": 3, "purple": 4}
-        self.idx_to_color = {0: "red", 1: "yellow", 2: "orange",
+        self.idx_to_color = {0: "red", 1: "orange", 2: "yellow",
                              3: "gray", 4: "purple"}
         # Env setup parameters
         self.colors_used = colors_used
@@ -63,12 +67,44 @@ class BartMetaEnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=1, shape=(5+1+2,))
         self.action_space = spaces.Discrete(2)  # 0: do nothing, 1: hold inflate button
 
+        # meta parameters
+        self.meta_setup = meta_setup
+        self.balloon_mean_sizes = {
+            0: None,
+            1: None,
+            2: None,
+            3: None,
+            4: None,
+        }
 
     def reset(self, seed=None, options={}):
         if seed is not None:
             np.random.seed(seed)
             random.seed(seed)
         
+        self.current_step = 0
+        
+        # generate meta params
+        if self.meta_setup == 0:
+            self.balloon_mean_sizes = {
+                0: np.random.uniform(0.1, 0.4),
+                1: np.random.uniform(0.4, 0.7),
+                2: np.random.uniform(0.7, 1)
+            }
+        elif self.meta_setup == 1:
+            self.balloon_mean_sizes = {
+                c: np.random.uniform(0.1, 1)
+                for c in range(5)
+            }
+
+        obs = self.inner_reset()
+        return obs, {}
+
+    def inner_reset(self):
+        '''
+        generate new balloon under current meta conditions
+        note that reset() changes to new meta conditions
+        '''
         if self.colors_used <= 1:
             self.current_color = "yellow"
         else:
@@ -76,20 +112,20 @@ class BartMetaEnv(gym.Env):
         self.current_size = 0.0
         self.prev_action = 0
         self.inflate_delay = 0
-        self.current_step = 0
-
+        
         self.currently_inflating = False # used for stop/start version
         
         # Pick a pop size
         mean = self.colors[self.current_color]["mean"]
         self.current_balloon_limit = random.gauss(mean, self.pop_noise)
-
-        return self.get_observation(), {}
+        
+        return self.get_observation()
 
     def step(self, action):
         terminated = False
         truncated = False
         popped = False
+        finished = False
         end_size = 0
         reward = 0
 
@@ -103,25 +139,18 @@ class BartMetaEnv(gym.Env):
                     end_size = self.current_size
                     self.current_size = 0  # Balloon pops
                     reward = self.rew_on_pop
-                    terminated = True
+                    finished = True
                     popped = True
-                # elif self.current_color in ["gray", "purple"]:
-                #     if self.current_size >= 20:  # Fixed size for passive trials
-                #         self.current_size = 20
-
             else:  # Action 0: stop inflating
                 if self.current_color in ["red", "yellow", "orange"]:
                     reward = self.current_size
-                # elif self.current_color in ["gray", "purple"]:
-                #     reward = self.colors[self.current_color]["fixed_reward"]
-                terminated = True
         else:
             if self.currently_inflating:
                 if action == 1:
                     if self.current_color in ["red", "yellow", "orange"]:
                         reward = self.current_size
                         self.currently_inflating = False
-                        terminated = True
+                        finished = True
                 if action == 0:
                     inflate = random.gauss(self.inflate_speed, self.inflate_noise)
                     self.current_size += inflate
@@ -131,7 +160,7 @@ class BartMetaEnv(gym.Env):
                         end_size = self.current_size
                         reward = self.rew_on_pop
                         self.currently_inflating = False
-                        terminated = True
+                        finished = True
                         popped = True
             else:
                 if action == 1:
@@ -147,6 +176,10 @@ class BartMetaEnv(gym.Env):
         else:
             last_size = self.current_size
             
+        if finished:
+            next_obs = self.inner_reset()
+        else:
+            next_obs = self.get_observation()
         # Note on max step termination, we will still give current size worth of points
         #   since this might better allow for reaction time in meta environment
         self.current_step += 1
@@ -159,10 +192,11 @@ class BartMetaEnv(gym.Env):
             'current_color': self.color_to_idx[self.current_color],
             'last_size': last_size,
             'popped': popped,
+            'bart_finished': finished,
             'inflate_delay': self.inflate_delay,
             'balloon_limit': self.current_balloon_limit
         }
-        return self.get_observation(), reward, terminated, truncated, info
+        return next_obs, reward, terminated, truncated, info
     
     def get_observation(self):
         obs = np.zeros(8, dtype=np.float32)
