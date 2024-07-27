3187178c43f15275ef1aea7b1ae888e9c02ac4fd
liuandrew <14andrewliu@gmail.com>
2024-07-27 12:49:00 -0700

    Fix to BartMetaEnv logging

--- main.py
+++ main.py
@@ -343,14 +343,13 @@ def main():
                         writer.add_scalar("bart/balloon_limit", info['balloon_limit'], global_step+n)
                     ep_bonus_reward[n] = 0
 
-
-                elif 'bart_finished' in info.keys():
+                # Logging for BartMetaEnv, where episode resets are infrequent
+                elif 'bart_finished' in info.keys() and info['bart_finished']:
                     writer.add_scalar("bart/color", info['current_color'], global_step+n)
                     writer.add_scalar("bart/size", info['last_size'], global_step+n)
                     writer.add_scalar("bart/popped", info['popped'], global_step+n)
                     writer.add_scalar("bart/inflate_delay", info['inflate_delay'], global_step+n)
                     writer.add_scalar("bart/balloon_limit", info['balloon_limit'], global_step+n)
-
 
 
             # If done then clean the history of observations.
